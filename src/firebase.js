4d4834eff5088778300bdb860977efb5cacfb0fb
Lindsay Lopez <lopez95@berkeley.edu>
2021-12-10 17:39:21 -0800

    Add firebase to project

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvHPDM0_q-LAGllOGJHgb41nfFoUEj6vM",
+  authDomain: "phototagging-a149e.firebaseapp.com",
+  projectId: "phototagging-a149e",
+  storageBucket: "phototagging-a149e.appspot.com",
+  messagingSenderId: "390536917352",
+  appId: "1:390536917352:web:03594f9f01f9f5c77a26dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
