9c74703c2a3d7487d5d6e1e858cd2d9518b685ad
Lindsay Lopez <lopez95@berkeley.edu>
2021-12-10 18:30:27 -0800

    Setup routes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 From The Odin Project
+
+Images from:
+Game 1: https://i.redd.it/z90c089eyyu61.jpg
+Game 2: https://external-preview.redd.it/RxnOVy8vII5L-R8vL0TfH74VTsbIQYoGyherLiJSZf0.jpg?auto=webp&s=3409f9423eb73139310eb755a282ecbd7e7bd20f
+Game 3: https://soranews24.com/wp-content/uploads/sites/3/2015/06/pika3.png
+Game 4: https://i.pinimg.com/originals/23/d4/85/23d4855c01fd62ee06dfff509e12667e.jpg
+Game 5: https://live.staticflickr.com/3249/5862194692_f6a4f87feb_b.jpg
+Game 6: https://lofi-gaming.org.uk/blog/wp-content/uploads/2016/06/wheressonic-3.jpg
--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Game from "./components/Game";
+import Home from "./components/Home";
+import Leaderboard from "./components/Leaderboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+          <Route path="/game" element={<Game />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
